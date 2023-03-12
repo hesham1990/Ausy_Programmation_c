7d9f07620b52c428a80ef6826025927cfe6a8fc1
Hesham_ALJBAAE <hesham.aljbaae@gmail.com>
2023-03-12 18:45:04 +0100

    le fichier entete.h

---
+++ entete.h
@@ -0,0 +1,15 @@
+#ifndef ENTETE_H_INCLUDED
+#define ENTETE_H_INCLUDED
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+void calculer_rendu_monnaie(int , int) ;
+
+
+
+#endif // ENTETE_H_INCLUDED
